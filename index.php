564d5e4792aabc3240f792af89233fb2c6aab175
ashrey <alberroteran@gmail.com>
2020-10-25 14:33:20 -0300

    Better look

--- index.php
+++ index.php
@@ -1,34 +1,61 @@
 <?php
 
-         $url = "https://api.kawalcorona.com/";
+$url = "https://api.kawalcorona.com/";
 
-         $client = curl_init($url);
+$client = curl_init($url);
 
-         curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
 
-         $response = curl_exec($client);
+$response = curl_exec($client);
 
-         $result = json_decode($response, true); // print_r($result); echo "<br>";
+$result = json_decode($response, true); // print_r($result); echo "<br>";
 
-         $countryRegion = $result[18]['attributes']['Country_Region'];
+$countryRegion = $result[18]['attributes']['Country_Region'];
 
-         $confirmed = $result[18]['attributes']['Confirmed']; 
+$confirmed = $result[18]['attributes']['Confirmed']; 
 
-         $deaths = $result[18]['attributes']['Deaths'];
+$deaths = $result[18]['attributes']['Deaths'];
 
-         $recovered = $result[18]['attributes']['Recovered'];
+$recovered = $result[18]['attributes']['Recovered'];
 
-         $dateTimeString = $result[18]['attributes']['Last_Update'] / 1000; 
-
-         $lastUpdate = date("l, d F Y H:i:s", $dateTimeString);
 $dateTimeString = $result[18]['attributes']['Last_Update'] / 1000; 
 
-         $lastUpdate = date("l, d F Y H:i:s", $dateTimeString);
+$lastUpdate = date("l, d F Y H:i:s", $dateTimeString);
+$dateTimeString = $result[18]['attributes']['Last_Update'] / 1000; 
 
-         echo "<center><b>Negara: ". $countryRegion;
+$lastUpdate = date("l, d F Y H:i:s", $dateTimeString);
+?>
 
-         echo "<br>";
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Data Statistik Covid 19 Indonesia</title>
+   <style type="text/css">
+      dl{
+         margin: auto;
+         max-width: 400px;
+      }
+      dt{
+         font-weight: bold;
+      }
 
-         echo "Positif : ". $confirmed;
+      img{
+         border: 1px solid #000;
+         max-width: 100px;
+      }
+   </style>
+</head>
+<body>
+   <dl>
+      <dt>Negara:</dt>
+      <dd><?=$countryRegion?></dd>
+      <dt>Flag:</dt>
+      <dt>
+         <img src="flag-indonesia.png" />
+      </dt>
+      <dt>Positif :</dt>
+      <dd><?=$confirmed?></dd>
+   </dl>
 
-        
+</body>
+</html>
