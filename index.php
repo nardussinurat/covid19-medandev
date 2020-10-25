1c9eb143ff84e1ee7e1dd1236dac7f0edd8eae58
nardus <56254027+nardussinurat@users.noreply.github.com>
2020-10-25 23:45:25 +0700

    add files from upload file
    adding index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+         $url = "https://api.kawalcorona.com/";
+
+         $client = curl_init($url);
+
+         curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+         $response = curl_exec($client);
+
+         $result = json_decode($response, true); // print_r($result); echo "<br>";
+
+         $countryRegion = $result[18]['attributes']['Country_Region'];
+
+         $confirmed = $result[18]['attributes']['Confirmed']; 
+
+         $deaths = $result[18]['attributes']['Deaths'];
+
+         $recovered = $result[18]['attributes']['Recovered'];
+
+         $dateTimeString = $result[18]['attributes']['Last_Update'] / 1000; 
+
+         $lastUpdate = date("l, d F Y H:i:s", $dateTimeString);
